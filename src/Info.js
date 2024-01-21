26c38a765c04a68e82bf1e4f268dc6bd66e8bb88
BusaidiEssa <essas.work@gmail.com>
2024-01-21 12:25:00 +0400

    renamed image and imported it into info

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { ReactDOM } from "react";
+import pfp from "./profile-pic.jpg"
 
 export default function Info(){
     <div className="info">
-        <img src="./asstes/profile pic.jpg"></img>
+        <img src="./asstes/profile pic.jpg" className="pfp" Alt="Profile picture"></img>
     </div>
 }
