990d966ece328d7739062851912bb498b74e6d53
BusaidiEssa <116034947+BusaidiEssa@users.noreply.github.com>
2024-01-21 16:50:18 +0400

    Update About.js

--- src/About.js
+++ src/About.js
@@ -7,8 +7,7 @@ export default function About(){
             <div className="content-container">
             <h3>About</h3>
             <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi temporibus, accusantium repellendus,
-            suscipit enim nulla commodi pariatur eius numquam in quisquam 
+            I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
             </p>
             </div>
         </div>
