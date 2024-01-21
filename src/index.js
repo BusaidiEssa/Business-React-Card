7b51718691a05ce8ee4690d59301fa5b727039af
BusaidiEssa <essas.work@gmail.com>
2024-01-21 11:54:03 +0400

    Imported components into index.js and rendered it

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import About from "./Info"
+import About from "./About"
+import About from "./Interests"
+import About from "./Footer"
 
 function Page(){
     return(
         <div>
+            <Info />
+            <About />
+            <Interests />
+            <Footer />
             
 
         </div>
     )
 }
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+  );
+  root.render(<Page />)
